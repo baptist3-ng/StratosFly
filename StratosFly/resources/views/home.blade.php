6a6073f04a3405685b8ac90724239ce2ffbc12cb
Bad4boum <186568477+baptist3-ng@users.noreply.github.com>
2025-03-11 23:48:57 +0100

    Création du template, création de /home

---
+++ StratosFly/resources/views/home.blade.php
@@ -0,0 +1,104 @@
+@extends('template')
+
+@section('titre', 'Home')
+
+@section('content')
+
+<section style="background-image: url('/images/ai_avion.webp'); background-size: cover; background-position: center; background-repeat: no-repeat; witdh: 1512px; height:661px">
+    <div class="container py-5">
+        <div class="row pt-5 my-2">
+            <div class="col-sm-4 offset-sm-7 offset-6 col-6 text-light fs-1 text-center fst-italic fw-bold pt-5">"Travel far, with peace of mind."</div>
+        </div>
+    </div>
+    
+</section>
+
+<section class="bg-light py-5">
+<div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card h-100">
+                    <img src="/images/porto.webp" class="card-img-top" alt="Portugal">
+                    <div class="card-body">
+                        <h5 class="card-title">Porto dès 59€* par personne !</h5>
+                        <p class="card-text">(*)Tarif par personne, hors taxes et frais. Offre soumise à conditions, voir détails sur notre site. Tarif applicable sur certaines dates et destinations uniquement.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card h-100">
+                    <img src="/images/budapest-la-nuit.webp" class="card-img-top" alt="Budapest">
+                    <div class="card-body">
+                        <h5 class="card-title">Budapest dès 75€* par personne !</h5>
+                        <p class="card-text">(*)Tarif par personne, hors taxes et frais. Offre soumise à conditions, voir détails sur notre site. Tarif applicable sur certaines dates et destinations uniquement.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card h-100">
+                    <img src="/images/chili.jpg" class="card-img-top" alt="Chili">
+                    <div class="card-body">
+                        <h5 class="card-title">Le Chili dès 395€* par personne !</h5>
+                        <p class="card-text">(*)Tarif par personne, hors taxes et frais. Offre soumise à conditions, voir détails sur notre site. Tarif applicable sur certaines dates et destinations uniquement.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+     <div class="container-fluid custom-color py-5">
+        <div class="row d-flex justify-content-evenly align-items-center">
+            <div class="col-sm-5 fs-1 fw-bold text-center py-5">
+                Pourquoi choisir StratosFly ?
+            </div>
+            <div class="col-sm-5 fw-bold py-5 text-center">
+                <div>- Tarifs attractifs et transparents : Profitez de prix compétitifs sans frais cachés.</div>
+                <div>- Service clientèle 24/7 : Toujours à votre écoute pour répondre à vos besoins.</div>
+                <div>- Confort et sécurité : Une flotte moderne pour un voyage agréable et sécurisé.</div>
+                <div>- Fidélité récompensée : Cumulez des points et bénéficiez d'avantages exclusifs.</div>
+                <div>- Réservation facile : Réservez en quelques clics, où que vous soyez.</div>
+            </div>
+        </div>
+     </div>
+</section>
+
+<section>
+<div class="container mt-5">
+        <div class="avis-container">
+            <div class="avis-title">Vos avis sont importants</div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="avis-card text-center">
+                        <div class="fs-4 py-2 fw-bold">4.3/5</div>
+                        <div class="star-rating">★★★★☆</div>
+                        <div class="review-text">Note moyenne sur +22k avis</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="avis-card">
+                        <div class="star-rating">★★★★★</div>
+                        <div class="review-text">"Très bon rapport qualité/prix."</div>
+                        <div class="reviewer-name">Gilbert</div>
+                        <div class="review-date">Publié le 19/03/25</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="avis-card">
+                        <div class="star-rating">★★★★★</div>
+                        <div class="review-text">"Excellent."</div>
+                        <div class="reviewer-name">Sophie</div>
+                        <div class="review-date">Publié le 22/01/25</div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-3">
+                <a href="#" class="text-decoration-none">Découvrir tous les avis</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+@endsection
---
+++ StratosFly/resources/views/template.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>@yield('title', 'AirPlane')</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+<header class="custom-color">
+    <nav class="navbar navbar-expand-md navbar-dark custom-color rounded-bottom-5 py-4">
+        <div class="container-fluid fw-bold">
+            <a class="navbar-brand fs-1 ps-5" href="#">Company Name</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto fs-4 pe-5">
+                    <li class="nav-item mx-lg-3"><a class="nav-link active" href="#">Billets</a></li>
+                    <li class="nav-item mx-lg-3"><a class="nav-link active" href="#">Contact</a></li>
+                    <li class="nav-item mx-lg-3"><a class="nav-link active" href="#">Mon compte</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+
+@yield('content')
+
+
+<footer class="custom-color">
+    <div class="container">
+        <div class="row justify-content-evenly py-4">
+            Company Name + Logo
+        </div>
+        <div class="row justify-content-evenly mx-4">
+            <div class="col-md-3 col-6">
+                <div style="font-size: extra-large" class="row py-2">
+                    Liens Utiles
+                </div>
+                <div style="font-size: medium" class="row">
+                    - Accueil<br>- A propos<br>- Réservations<br>- Contact<br>- FAQ 
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div style="font-size: extra-large" class="row py-2">
+                    Contact
+                </div>
+                <div style="font-size: medium" class="row">
+                    - support@company.com<br>- +123 456 789
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div style="font-size: extra-large" class="row py-2">
+                    Suivez-nous
+                </div>
+                <div class="row">
+                    
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div style="font-size: extra-large" class="row py-2">
+                    Newsletter
+                </div>
+                <div style="font-size: medium" class="row">
+                    - Inscrivez-vous pour recevoir nos dernières offres et actualités.
+                </div>
+            </div>
+        </div>
+        <div class="row py-4 d-flex justify-content-center">
+            © 2025 AirPlane INC - Tous droits réservés
+        </div>
+    </div>
+</footer>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
