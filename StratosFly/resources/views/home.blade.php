98e5f9f411ad212a282e00d3b614319495420ab1
Bad4boum <186568477+baptist3-ng@users.noreply.github.com>
2025-03-12 14:03:06 +0100

    Ajout du formulaire, fix responsive

--- StratosFly/resources/views/home.blade.php
+++ StratosFly/resources/views/home.blade.php
@@ -4,17 +4,61 @@
 
 @section('content')
 
-<section style="background-image: url('/images/ai_avion.webp'); background-size: cover; background-position: center; background-repeat: no-repeat; witdh: 1512px; height:661px">
+<section style="background-image: url('/images/ai_avion.webp'); background-size: cover; background-position: center; background-repeat: no-repeat; witdh: 1512px; height:661px" class="pb-5">
     <div class="container py-5">
         <div class="row pt-5 my-2">
             <div class="col-sm-4 offset-sm-7 offset-6 col-6 text-light fs-1 text-center fst-italic fw-bold pt-5">"Travel far, with peace of mind."</div>
+        </div>
+    </div>
+    <div class="container bg-secondary-subtle rounded-4 pb-3">
+        <div class="row">
+            <div class="col-lg-2 custom-color col-md-4 col-6 rounded-bottom-3 ps-4">
+                Acheter un billet
+            </div>
+        </div>
+        <div class="row">
+            <form action="" class="d-lg-flex justify-content-evenly">
+                <div class="col-lg-2 py-lg-4 py-2 col-12">
+                    <select id="trajet" name="trajet" class="p-lg-3 form-select ">
+                        <option value="aller-retour" selected>Aller-Retour</option>
+                        <option value="aller">Aller</option>
+                        <option value="retour">Retour</option>
+                    </select>
+                </div>
+                <div class="col-lg-3 py-lg-4 py-2 col-12">
+                    <select id="depart" name="depart" class="p-lg-3 form-select">
+                        <option value="default" disabled selected>Départ de </option>
+                        <option value="a1">Aéroport Francisco Sá Carneiro, Portugal.</option>
+                        <option value="a2">Aéroport Roissy Charles de Gaulle, France.</option>
+                        <option value="a3">Aéroport Francisco Sá Carneiro, Portugal.</option>
+                        <option value="a4">Aéroport Roissy Charles de Gaulle, France.</option>
+                    </select>
+                </div>
+                <div class="col-lg-3 py-lg-4 py-2 col-12">
+                    <select id="arrivee" name="arrivee" class="p-lg-3 form-select">
+                        <option value="default" disabled selected>Arrivée à </option>
+                        <option value="a1">Aéroport Francisco Sá Carneiro, Portugal.</option>
+                        <option value="a2">Aéroport Roissy Charles de Gaulle, France.</option>
+                        <option value="a3">Aéroport Francisco Sá Carneiro, Portugal.</option>
+                        <option value="a4">Aéroport Roissy Charles de Gaulle, France.</option>
+                    </select>
+                </div>
+                <div class="col-lg-2 py-lg-4 py-2 col-12">
+                    <input type="submit" value="PARTIR" class="p-3 btn custom-color col-12">
+                </div>
+            </form>
         </div>
     </div>
     
 </section>
 
 <section class="bg-light py-5">
-<div class="container mt-5">
+<div class="container">
+    <div class="row text-center py-5">
+        <div class="fs-1 fw-bold">
+            Quelle est votre prochaine aventure ?
+        </div>
+    </div>
         <div class="row">
             <div class="col-md-4 mb-4">
                 <div class="card h-100">
@@ -53,7 +97,7 @@
             <div class="col-sm-5 fs-1 fw-bold text-center py-5">
                 Pourquoi choisir StratosFly ?
             </div>
-            <div class="col-sm-5 fw-bold py-5 text-center">
+            <div class="col-sm-5 fw-bold py-5 text-center fs-5">
                 <div>- Tarifs attractifs et transparents : Profitez de prix compétitifs sans frais cachés.</div>
                 <div>- Service clientèle 24/7 : Toujours à votre écoute pour répondre à vos besoins.</div>
                 <div>- Confort et sécurité : Une flotte moderne pour un voyage agréable et sécurisé.</div>
